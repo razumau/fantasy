8d46784dfa188d33ed2ab6bcc05d29642808e978
Jury Razumau <mail@razumau.net>
2024-03-23 18:01:23 +0100

    add initial clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata = {
   metadataBase: new URL('https://postgres-prisma.vercel.app'),
@@ -20,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+      <body>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
