51ea559ff7609f0a5e088d4cfff2976c341e5aff
Jury Razumau <mail@razumau.net>
2024-03-31 23:20:21 +0200

    stop recreating prisma client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { auth, clerkClient, UserButton } from "@clerk/nextjs";
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/lib/prisma'
 
 export default async function Home() {
     const { userId } = auth();
