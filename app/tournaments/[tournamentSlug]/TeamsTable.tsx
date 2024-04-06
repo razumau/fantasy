6c40af9208c764dd723b9e991ae3515f443b8a7e
Jury Razumau <mail@razumau.net>
2024-04-06 23:40:45 +0200

    remove checkboxes from closed tournaments

--- app/tournaments/[tournamentSlug]/TeamsTable.tsx
+++ app/tournaments/[tournamentSlug]/TeamsTable.tsx
@@ -6,25 +6,31 @@ type TeamsTableProps = {
     teams: Team[];
     selectedTeamIds: number[];
     handleCheckboxChange: (selectedTeam: Team) => void;
+    isOpen: boolean;
 }
 
-export default function TeamsTable({ teams, selectedTeamIds, handleCheckboxChange }: TeamsTableProps) {
+export default function TeamsTable({ teams, selectedTeamIds, handleCheckboxChange, isOpen }: TeamsTableProps) {
+    const buildTableRow = (team: Team) =>{
+        const checkbox = isOpen ?
+            <Checkbox
+                isChecked={selectedTeamIds.some(id => id === team.id)}
+                onChange={(_e) => handleCheckboxChange(team)}
+            /> : null;
+
+        return <Tr key={team.id} _hover={{background: "#ebf8ff"}}>
+            <Td>
+                {checkbox}
+            </Td>
+            <Td>{team.name}</Td>
+            <Td isNumeric>{team.price}</Td>
+        </Tr>
+    }
+
     return (
         <TableContainer>
             <Table variant='simple'>
                 <Tbody>
-                    {teams.map((team) => (
-                        <Tr key={team.id} _hover={{background: "#ebf8ff"}}>
-                            <Td>
-                                <Checkbox
-                                    isChecked={selectedTeamIds.some(id => id === team.id)}
-                                    onChange={(_e) => handleCheckboxChange(team)}
-                                />
-                            </Td>
-                            <Td>{team.name}</Td>
-                            <Td isNumeric>{team.price}</Td>
-                        </Tr>
-                    ))}
+                    {teams.map((team) => buildTableRow(team))}
                 </Tbody>
             </Table>
         </TableContainer>
