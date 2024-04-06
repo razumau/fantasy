a7156d80ed6e80d87dbbbb5d0c52edeb9d08a042
Jury Razumau <mail@razumau.net>
2024-04-06 23:40:31 +0200

    add teams to test tournaments

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Tournament } from '@prisma/client'
 const prisma = new PrismaClient()
 
-async function main() {
-    const user = await prisma.user.upsert({
+async function createUser() {
+    await prisma.user.upsert({
         where: { clerkId: 'user_2e67b28OTBXFrAy8H3p6vRYlUQe' },
         update: {},
         create: {
@@ -11,7 +11,9 @@ async function main() {
             isAdmin: true,
         },
     });
+}
 
+async function createTournaments() {
     const tournament = await prisma.tournament.upsert({
         where: { slug: "pl-2024" },
         update: {},
@@ -46,7 +48,43 @@ async function main() {
             maxPrice: 100,
             maxTeams: 3,
         }
-    })
+    });
+
+    return [tournament, testOne, testTwo];
+}
+
+async function createTeams(tournamentId: number, withPoints: boolean = false) {
+    const teams: [string, number][] = [
+        ["Team 1", 50],
+        ["Team 2", 40],
+        ["Team 3", 40],
+        ["Team 4", 30],
+        ["Team 5", 30],
+        ["Team 6", 30],
+        ["Team 7", 20],
+        ["Team 8", 20],
+        ["Team 9", 15],
+        ["Team 10", 15],
+        ["Team 11", 15],
+        ["Team 12", 15],
+        ["Team 13", 15],
+    ]
+    for (const [name, price] of teams) {
+        const points = withPoints ? price : 0;
+
+        const team = await prisma.team.findFirst({ where: { tournamentId, name, price, points} });
+
+        if (!team) {
+            await prisma.team.create({ data: { tournamentId, name, price, points } })
+        }
+    }
+}
+
+async function main() {
+    await createUser();
+    const [, testOne, testTwo] = await createTournaments();
+    await createTeams(testOne.id, false);
+    await createTeams(testTwo.id, true);
 }
 
 main()
