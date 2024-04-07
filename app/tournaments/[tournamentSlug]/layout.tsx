f540da7f3cb951a96f132ec7de691fc0f8b32a90
Jury Razumau <mail@razumau.net>
2024-04-08 00:11:28 +0200

    add footer

--- app/tournaments/[tournamentSlug]/layout.tsx
+++ app/tournaments/[tournamentSlug]/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Flex} from "@chakra-ui/react";
+import {Box, Flex, Text, Link, Divider} from "@chakra-ui/react";
 import {UserButton} from "@clerk/nextjs";
 
 export default function TournamentLayout({children}: {
@@ -11,6 +11,13 @@ export default function TournamentLayout({children}: {
                 <UserButton/>
             </Flex>
             {children}
+            <Divider></Divider>
+            <Flex as="footer" width="full" align="center" justifyContent="center" p={4}>
+                <Text mr={2}>Made by Jury Razumau.</Text>
+                <Link href="https://www.buymeacoffee.com/juryrazumau" isExternal>
+                    You can buy me a coffee.
+                </Link>
+            </Flex>
         </Box>
     )
 }
