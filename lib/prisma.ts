27ee027ceac204a580757b87e844e309e8abf5d6
Jury Razumau <mail@razumau.net>
2024-04-09 23:15:27 +0200

    set production pragmas

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,4 +8,8 @@ const prisma = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
+prisma.$queryRaw`PRAGMA journal_mode = WAL;`;
+prisma.$queryRaw`PRAGMA synchronous = NORMAL;`;
+prisma.$queryRaw`PRAGMA busy_timeout = 5000;`;
+
 export default prisma
