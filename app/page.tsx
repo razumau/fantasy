a1eb7a3e42ca8bf1ad45cd016bbac33ed881e00b
Jury Razumau <mail@razumau.net>
2024-05-01 17:25:20 +0200

    fix layout for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Container, Box, Heading, Link} from '@chakra-ui/react';
+import {Container, Box, Heading, Link, Text} from '@chakra-ui/react';
 import NextLink from "next/link";
 import React from "react";
 
@@ -6,11 +6,11 @@ export default function PrivacyPolicy() {
     return (
         <Container maxW='4xl' centerContent>
             <Box maxW='2xl'>
-                <Heading py='12' as='h2' size='lg'>These tournaments are active:</Heading>
-                <Link pt='8' as={NextLink} href={`/tournaments/schr-2024`}>СЧР 2024</Link>
-                <Heading py='12' as='h2' size='lg'>See results for completed tournaments:</Heading>
-                <Link pt='8' as={NextLink} href={`/tournaments/pl-2024/results`}>Чемпионат Польши 2024</Link>
-                <Link pt='8' as={NextLink} href={`/tournaments/shchr-2024/results`}>ШЧР 2024</Link>
+                <Heading pb='2' as='h2' size='lg'>These tournaments are active:</Heading>
+                <Text pt='2'><Link as={NextLink} href={`/tournaments/schr-2024`}>СЧР 2024</Link></Text>
+                <Heading pt='4' pb='2' as='h2' size='lg'>See results for completed tournaments:</Heading>
+                <Text pt='2'><Link as={NextLink} href={`/tournaments/pl-2024/results`}>Чемпионат Польши 2024</Link></Text>
+                <Text pt='2'><Link as={NextLink} href={`/tournaments/shchr-2024/results`}>ШЧР 2024</Link></Text>
             </Box>
         </Container>
     );
