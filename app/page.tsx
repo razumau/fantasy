c6766a2d5079a496606f69c603c63702734446fd
Jury Razumau <mail@razumau.net>
2024-05-01 17:16:40 +0200

    update list of active tournaments

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,11 @@ export default function PrivacyPolicy() {
     return (
         <Container maxW='4xl' centerContent>
             <Box maxW='2xl'>
-                <Heading as='h2' size='lg'>These tournaments are active:</Heading>
-                <Link pt='8' as={NextLink} href={`/tournaments/shchr-2024`}>ШЧР 2024</Link>
-                <Heading as='h2' size='lg'>See results for completed tournaments:</Heading>
+                <Heading py='12' as='h2' size='lg'>These tournaments are active:</Heading>
+                <Link pt='8' as={NextLink} href={`/tournaments/schr-2024`}>СЧР 2024</Link>
+                <Heading py='12' as='h2' size='lg'>See results for completed tournaments:</Heading>
                 <Link pt='8' as={NextLink} href={`/tournaments/pl-2024/results`}>Чемпионат Польши 2024</Link>
+                <Link pt='8' as={NextLink} href={`/tournaments/shchr-2024/results`}>ШЧР 2024</Link>
             </Box>
         </Container>
     );
