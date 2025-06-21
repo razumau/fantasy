442f434d508f72e9d9d8243874861adc50335ddf
Jury Razumau <mail@razumau.net>
2025-06-21 23:07:24 +0200

    add missing migration
    972cc9c5563fe1099e79ec54570b635433ad0c05 didn’t have a migration

---
+++ prisma/migrations/20250621210626_add_spreadsheet_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Tournament" ADD COLUMN "resultColumnName" TEXT;
+ALTER TABLE "Tournament" ADD COLUMN "spreadsheetUrl" TEXT;
+ALTER TABLE "Tournament" ADD COLUMN "teamColumnName" TEXT;
